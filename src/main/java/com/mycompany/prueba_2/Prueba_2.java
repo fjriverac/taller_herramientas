0652553c3437f6af2442b37dbbb3926a14e54f92
f1j2r <f1j2r@192.168.1.69>
2024-03-19 14:28:16 -0600

    updated

---
+++ src/main/java/com/mycompany/prueba_2/Prueba_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prueba_2;
+
+/**
+ *
+ * @author f1j2r
+ */
+public class Prueba_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
